9b28d5bee92afd9fa1728a816b49b9d1e11aef59
matheusburey <matheushburei.99@gmail.com>
2025-06-26 19:17:51 -0300

    feat: create button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import {
+	Text,
+	TouchableOpacity,
+	type TouchableOpacityProps,
+} from "react-native";
+
+type ButtonProps = TouchableOpacityProps & {
+	title: string;
+};
+
+export default function Button({ title, ...rest }: ButtonProps) {
+	return (
+		<TouchableOpacity
+			className="bg-purple-500 h-12 w-full rounded-lg items-center justify-center"
+			activeOpacity={0.8}
+			{...rest}
+		>
+			<Text className="text-white text-lg font-bold">{title}</Text>
+		</TouchableOpacity>
+	);
+}
