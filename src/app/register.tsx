ca0820e1231205a841f429d4bab483f08858fed2
matheusburey <matheusbureidev@gmail.com>
2025-07-23 11:42:01 -0300

    style: fix form in center

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -56,7 +56,7 @@ export default function RegisterScreen() {
 
 	return (
 		<View className="flex-1 bg-gray-100 dark:bg-zinc-800 justify-center">
-			<View className="w-full max-w-md px-10 gap-4">
+			<View className="w-full max-w-md mx-auto px-10 gap-4">
 				<Text className="text-3xl dark:text-zinc-200 font-bold">
 					{t("register.title")}
 				</Text>
